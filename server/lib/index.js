cb3324ab910d4c2ad21cdb4057a5d499ad64c4a4
Leo Nicolle <leo@linkurio.us>
2022-01-29 13:03:14 +0100

    create app file for further unit tests

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,34 +1,3 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import { existsSync } from "fs";
-import db from "./database";
-import wordController from "./controllers/wordController";
-import gridController from "./controllers/gridController";
-import searchController from "./controllers/search-controller";
-import dico from "./search/dico";
+import createApp from "./app"
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-if (existsSync("public")) {
-  console.log("server static");
-  app.use(express.static("public"));
-}
-db.getWords().then((words) => {
-  dico.addWordsToDictionnary(words);
-});
-wordController({ app, db });
-gridController({ app, db });
-searchController({ app, db });
-
-if (require.main === module) {
-  const server = app.listen(+process.env.APP_CROSSWORDS_PORT || 3010, () => {
-    console.log(
-      `server running at port http://localhost/${server.address().port}`
-    );
-  });
-}
-export default app;
+createApp();
--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,34 +1,31 @@
-
-import pkg from './package.json';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import replace from '@rollup/plugin-replace';
-import dotenv from 'dotenv';
-import typescript from '@rollup/plugin-typescript';
+import pkg from "./package.json";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import replace from "@rollup/plugin-replace";
+import dotenv from "dotenv";
+import typescript from "@rollup/plugin-typescript";
 
 dotenv.config();
-console.log('building in mode:', process.env.MODE)
+console.log("building in mode:", process.env.MODE);
 const variablesToReplace = Object.entries(process.env)
-	.filter(([key]) => key.match(/APP_CROSSWORDS_.+/))
-	.reduce((variablesToReplace, [key, value]) => {
-		variablesToReplace[`process.env.${key}`] = JSON.stringify(value)
-		return variablesToReplace;
-	}, {})
-	console.log('variables to replace:', variablesToReplace)
+  .filter(([key]) => key.match(/APP_CROSSWORDS_.+/))
+  .reduce((variablesToReplace, [key, value]) => {
+    variablesToReplace[`process.env.${key}`] = JSON.stringify(value);
+    return variablesToReplace;
+  }, {});
+console.log("variables to replace:", variablesToReplace);
 
-	const plugins =[
-		replace(variablesToReplace),
-		resolve(),
-		json(),
-		typescript(),
-		commonjs(),
-	]
+const plugins = [
+  replace(variablesToReplace),
+  resolve(),
+  json(),
+  typescript(),
+  commonjs(),
+];
 
 export default {
-	input: 'lib/index.js',
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-	],
-	plugins
+  input: "lib/index.js",
+  output: [{ file: pkg.main, format: "cjs" }],
+  plugins,
 };
