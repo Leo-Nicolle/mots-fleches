a552b1ebd4fbbe8f9a433a89f35a4f03332ef8bb
Leo Nicolle <leo@linkurio.us>
2024-02-17 15:27:41 +0100

    try to test select element

--- client/test/forms/Sizeinput.test.ts
+++ client/test/forms/Sizeinput.test.ts
@@ -1,16 +1,10 @@
 import { describe, expect, it } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { mount, wait } from '../utils';
+import Vue from 'vue';
 import Sizeinput from '../../src/components/forms/Sizeinput.vue';
-import { NInputNumber, NSelect } from 'naive-ui';
 describe.only('Sizeinput', () => {
   it('Should update modelValue', async () => {
     const wrapper = mount(Sizeinput, {
-      global: {
-        components: {
-          'n-input-number': NInputNumber,
-          'n-select': NSelect,
-        },
-      },
       props: {
         modelValue: '10cm',
         'onUpdate:modelValue': (e) => wrapper.setProps({ modelValue: e }),
@@ -18,8 +12,20 @@ describe.only('Sizeinput', () => {
     });
     // get input type number
     const input = wrapper.find('input[type=text]');
+    document.body.appendChild(input.element);
     await input.setValue(20);
-    expect(wrapper.props('modelValue')).toBe('20cm');
+
+    // const select = wrapper.find('.n-select>div');
+    // await select.trigger('click');
+    await wrapper.find('.n-base-selection').trigger('click');
+    await wait(100);
+    debugger;
+    const a = document.querySelector('.v-vl-visible-items');
+    console.log(a);
+    // const option = wrapper.findAll('.n-base-select-option')
+    //   .find((option) => option.text() === 'pt');
+    // await option.trigger('click');
+    expect(wrapper.props('modelValue')).toBe('20pt');
     // TODO: find a way to test select
     // const select = wrapper.find('select');
     // await select.setValue('pt');
